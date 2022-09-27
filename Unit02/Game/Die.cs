a88a1a98508b73211ea87c2f6006fd6f0f821469
Michael Hendrick <mhendrick@byui.edu>
2022-09-27 09:44:19 -0600

    Classwork

--- Unit02/Game/Die.cs
+++ Unit02/Game/Die.cs
@@ -6,6 +6,8 @@ namespace Unit02.Game
     // TODO: Implement the Die class as follows...
     // 1) Add the class declaration. Use the following class comment.
 
+    class Die
+    {
         /// <summary>
         /// A small cube with a different number of spots on each of its six sides.
         /// 
@@ -13,19 +15,41 @@ namespace Unit02.Game
         /// worth.
         /// </summary> 
 
+        public int points;
+        public int value;
 
     // 2) Create the class constructor. Use the following method comment.
 
+        public void Book() { 
         /// <summary>
         /// Constructs a new instance of Die.
         /// </summary>
 
+
+        }
     
     // 3) Create the Roll() method. Use the following method comment.
         
+        public void Roll() {
         /// <summary>
         /// Generates a new random value and calculates the points for the die. Fives are 
         /// worth 50 points, ones are worth 100 points, everything else is worth 0 points.
         /// </summary>
-        
+
+        Random rnd = new Random();
+        value = rnd.Next(1, 7);
+
+        if (value == 5) {
+            points = 50;
+        }
+        else if (value == 1) {
+            points = 100;
+        }
+        else {
+            points = 0;
+        }
+
+        }
+
+    }   
 }
