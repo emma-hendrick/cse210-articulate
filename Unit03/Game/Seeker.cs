d4cd711c569c53f3eb062bdd92b0e7c8e1322d4f
Michael Hendrick <mhendrick@byui.edu>
2022-10-11 15:37:11 -0600

    Unit 03 Articulate

--- Unit03/Game/Seeker.cs
+++ Unit03/Game/Seeker.cs
@@ -7,12 +7,15 @@ namespace Unit03.Game
 
     // 1) Add the class declaration. Use the following class comment.
 
-        /// <summary>
-        /// <para>The person looking for the Hider.</para>
-        /// <para>
-        /// The responsibility of a Seeker is to keep track of its location.
-        /// </para>
-        /// </summary>
+    /// <summary>
+    /// <para>The person looking for the Hider.</para>
+    /// <para>
+    /// The responsibility of a Seeker is to keep track of its location.
+    /// </para>
+    /// </summary>
+    public class Seeker
+    {
+        private int _location;
 
 
     // 2) Create the class constructor. Use the following method comment.
@@ -20,6 +23,10 @@ namespace Unit03.Game
         /// <summary>
         /// Constructs a new instance of Seeker.
         /// </summary>
+        public Seeker() 
+        {
+            _location = 1000;
+        }
        
 
     // 3) Create the GetLocation() method. Use the following method comment.
@@ -28,6 +35,10 @@ namespace Unit03.Game
         /// Gets the current location.
         /// </summary>
         /// <returns>The current location as an int.</returns>
+        public int GetLocation() 
+        {
+            return _location;
+        }
         
 
     // 4) Create the MoveLocation(int location) method. Use the following method comment.
@@ -36,5 +47,9 @@ namespace Unit03.Game
         /// Moves to the given location.
         /// </summary>
         /// <param name="location">The given location.</param>
-    
+        public void MoveLocation(int location) 
+        {
+            _location = location;
+        }
+    }
 }
